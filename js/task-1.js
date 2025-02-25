7e942dc2f192e7a341d01fffac685f28a9e5abc0
Bohdan Vasylovych <war.mant1kor@gmail.com>
2025-02-25 12:32:37 +0200

    GoIt JS HW #2

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,10 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
-    const totalPrice = quantity * pricePerDroid;
-    if (customerCredits < totalPrice) {
-        return `"Insufficient funds!"`;
-    } else {
-        return `"You ordered ${quantity} droids worth ${totalPrice} credits!"`
-    }
+  const totalPrice = quantity * pricePerDroid;
+  if (customerCredits < totalPrice) {
+    return `"Insufficient funds!"`;
+  } else {
+    return `"You ordered ${quantity} droids worth ${totalPrice} credits!"`;
+  }
 }
 
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
@@ -12,7 +12,6 @@ console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 300
 console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
 console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
 console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
-
 
 // Задача 1. Замовлення дроїдів
 // #region Task Conditions
@@ -32,4 +31,3 @@ console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 500
 // в іншому випадку функція має повертати рядок "You ordered <quantity> droids worth <totalPrice> credits!", де    <quantity> це кількість замовлених дроїдів, а <totalPrice> це їх загальна вартість.
 // Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
 // #endregion
-/
